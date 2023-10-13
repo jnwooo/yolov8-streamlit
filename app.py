28961f818bfefc161eb60360157d14b5a07b5f85
Ong Aun Jie <ongaunjie1@gmail.com>
2023-10-14 00:41:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ import PIL
 # External packages
 import streamlit as st
 import easyocr  
+reader = easyocr.Reader(['en'], gpu=False) 
 import cv2
 import numpy as np
 
 # Local Modules
 import settings
 import helper
+
 
 # Setting page layout
 st.set_page_config(
@@ -23,15 +25,25 @@ st.set_page_config(
 # Main page heading
 st.title("Object Detection using YOLOv8")
 
-# Sidebar
+# Sidebar header
 st.sidebar.header("ML Model Config")
 
 # Model Options
 model_type = st.sidebar.radio(
     "Select Task", ['Detection', 'Segmentation', 'Potholes Detection', 'License Plate Detection' , 'License Plate Detection with EasyOCR', 'PPE Detection'])
 
+# Sidebar slider
 confidence = float(st.sidebar.slider(
     "Select Model Confidence", 25, 100, 40)) / 100
+if model_type == 'License Plate Detection with EasyOCR':
+    # Slider for floodfill threshold
+    floodfill_threshold = st.sidebar.slider('Floodfill Threshold', 0, 250, 100, step=1)
+
+    # Slider for thresholding block size
+    threshold_block_size = st.sidebar.slider('Threshold Block Size (odd number, > 1)', 3, 201, 101, step=2)
+
+    # Slider for brightness adjustment
+    brightness = st.sidebar.slider('Brightness Adjustment', -100, 100, 0, step=1)
 
 # Selecting Detection Or Segmentation
 if model_type == 'Detection':
@@ -92,79 +104,84 @@ if source_radio == settings.IMAGE:
             if st.sidebar.button('Detect Objects'):
                 res = model.predict(uploaded_image, conf=confidence)
                 boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',
-                         use_column_width=True)
-                try:
-                    with st.expander("Detection Results"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    st.write("No image is uploaded yet!")
+                if len(boxes) == 0:
+                    st.error("No objects detected in the image.")
+                else:
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Detected Image',
+                            use_column_width=True)
+                    try:
+                        with st.expander("Detection Results"):
+                            for box in boxes:
+                                st.write(box.data)
+                    except Exception as ex:
+                        st.write("No image is uploaded yet!")
 
         if source_img is not None and model_type == 'License Plate Detection with EasyOCR':
             if st.sidebar.button('Read License Plate'):
-                # Perform OCR on the detected number plate regions
-                # Ensure that you have the coordinates of the number plate regions (e.g., 'license_plates')
-                reader = easyocr.Reader(['en'], gpu=False)  # You can specify the language
+            # Read and display the uploaded image
+                original_image = PIL.Image.open(source_img)
+                original_image = np.array(original_image)
 
                 # Detect license plates
-                license_plates = model.predict(uploaded_image, conf=confidence)
+                license_plates = model.predict(original_image)
 
-                # Create a copy of the original image to modify
-                modified_image = np.array(uploaded_image)
+                if not len(license_plates[0].boxes) == 0:
+                    for i, license_plate in enumerate(license_plates[0].boxes):
+                        x1, y1, x2, y2 = license_plate.xyxy[0]
 
-                for i, license_plate in enumerate(license_plates[0].boxes.data.tolist()):
-                    x1, y1, x2, y2, score, class_id = license_plate
+                        # Crop license plate from the original image
+                        license_plate_image = original_image[int(y1):int(y2), int(x1):int(x2)]
 
-                    # Process license plate
-                    license_plate_image_gray = cv2.cvtColor(modified_image[int(y1):int(y2), int(x1):int(x2), :], cv2.COLOR_BGR2GRAY)
-                    _, license_plate_image_thresh = cv2.threshold(license_plate_image_gray, 64, 255, cv2.THRESH_BINARY_INV)
+                        # Process the license plate image with adjustable parameters
+                        processed_license_plate = helper.process_license_plate(license_plate_image, floodfill_threshold, threshold_block_size, brightness)
 
-                    # Read license plate image
-                    detections = reader.readtext(license_plate_image_thresh)
-
+                    #Perform OCR on the processed license plate image
+                    detections = reader.readtext(processed_license_plate)
                     if detections:
-                        detected_plate_text = detections[0][1]  # Extract the detected text
+                            detected_plate_text = detections[0][1]  # Extract the detected text
 
-                        # Replace the class name with the detected license plate text
-                        license_plate_text = detected_plate_text
+                            # Replace the class name with the detected license plate text
+                            license_plate_text = detected_plate_text
 
-                        # Draw a bounding box around the detected car plate
-                        cv2.rectangle(modified_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+                            # Draw a bounding box around the detected car plate
+                            cv2.rectangle(original_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
 
-                        # Calculate the text size
-                        text_size, _ = cv2.getTextSize(license_plate_text, cv2.FONT_HERSHEY_SIMPLEX, 1.3, 2)
+                            # Calculate the text size
+                            text_size, _ = cv2.getTextSize(license_plate_text, cv2.FONT_HERSHEY_SIMPLEX, 1.3, 2)
 
-                        # Calculate the position to center the text
-                        text_x = int(x1 + (x2 - x1 - text_size[0]) / 2)
-                        text_y = int(y1 - 10)
+                            # Calculate the position to center the text
+                            text_x = int(x1 + (x2 - x1 - text_size[0]) / 2)
+                            text_y = int(y1 - 10)
 
-                        # Calculate the background size (adjust as needed)
-                        background_width = text_size[0] + 20  # Add some extra space for padding
-                        background_height = text_size[1] + 10  # Add some extra space for padding
+                            # Calculate the background size (adjust as needed)
+                            background_width = text_size[0] + 20  # Add some extra space for padding
+                            background_height = text_size[1] + 10  # Add some extra space for padding
 
-                        # Calculate the position for the background
-                        background_x1 = text_x - 10
-                        background_y1 = text_y - text_size[1] - 5  # Adjusted to be higher
-                        background_x2 = background_x1 + background_width
-                        background_y2 = text_y + 5  # Adjusted to be lower
+                            # Calculate the position for the background
+                            background_x1 = text_x - 10
+                            background_y1 = text_y - text_size[1] - 5  # Adjusted to be higher
+                            background_x2 = background_x1 + background_width
+                            background_y2 = text_y + 5  # Adjusted to be lower
 
-                        # Draw the enlarged filled background for the text
-                        background_color = (0, 0, 0)
-                        cv2.rectangle(modified_image, (background_x1, background_y1), (background_x2, background_y2), background_color, -1)
+                            # Draw the enlarged filled background for the text
+                            background_color = (0, 0, 0)
+                            cv2.rectangle(original_image, (background_x1, background_y1), (background_x2, background_y2), background_color, -1)
 
-                        # Draw the centered text on the enlarged filled background
-                        text_color = (255, 255, 255)  # Text color
-                        cv2.putText(modified_image, license_plate_text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1.3, text_color, 2)
+                            # Draw the centered text on the enlarged filled background
+                            text_color = (255, 255, 255)  # Text color
+                            cv2.putText(original_image, license_plate_text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1.3, text_color, 2)
+               
+                    # Convert the modified image back to PIL format for displaying with Streamlit
+                    image_with_text = PIL.Image.fromarray(original_image)
 
-
-                # Convert the modified image back to PIL format for displaying with Streamlit
-                image_with_text = PIL.Image.fromarray(modified_image)
-
-                # Display the modified image with updated bounding boxes and license plate text
-                st.image(image_with_text, caption='Detected License Plate',
-                        use_column_width=True)
+                    # Display the modified image with updated bounding boxes and license plate text
+                    st.image(image_with_text, caption='Detected License Plate',
+                            use_column_width=True)  
+                    # Display the processed license plate image
+                    st.image(processed_license_plate, caption='Processed License Plate', use_column_width=True)
+                else:
+                  st.error("No license plates detected in the image.")
           
 elif source_radio == settings.VIDEO:
     if model_type == 'License Plate Detection with EasyOCR':
